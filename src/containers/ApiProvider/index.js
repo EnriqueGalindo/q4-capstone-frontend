f9b5d6e31d1c2eb0f791069e55d5cc7dea4ade62
James Maupin <jmsMaupinRC@gmail.com>
2020-04-07 13:35:00 -0400

    Move components to containers and create encounter list item

--- src/containers/ApiProvider/index.js
+++ src/containers/ApiProvider/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ApiProvider({children}) {
 
-    const BASE_URL = ''
+    const BASE_URL = 'localhost:800/api'
 
     return children.map( (child, index) => {
         return React.cloneElement(child, {
--- src/containers/EncounterList/index.js
+++ src/containers/EncounterList/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+// How should we sort and filter the encounters to make specific ones
+// easier to find for the DM
 export default function EncounterList() {
     return (
         <div>
